50405777551d02bfd43483c23936840a3625ff65
maracodez <amarachiruhty4@gmail.com>
2025-07-12 02:35:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// This function is triggered when the user clicks on the email link
+function sendEmail() {
+    window.location.href = "mailto:nyechiscouture@example.com";
+}
+// This function initiates a phone call when the user clicks on the phone number
+function callUs() {
+    window.location.href = "tel:+2349071577490"; 
+}
